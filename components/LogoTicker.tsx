9aa94bce3a1b07c2beb248b04fbdec360eb49d89
Walison Theodoro <theodoro.walison@gmail.com>
2024-08-10 20:07:28 -0300

    feat: add logo ticker animation

--- components/LogoTicker.tsx
+++ components/LogoTicker.tsx
@@ -1,48 +1,79 @@
+'use client'
+
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from 'framer-motion'
+
 import Image from 'next/image'
 import acmeLogo from '@/assets/logo-acme.png'
 import apexLogo from '@/assets/logo-apex.png'
 import celestialLogo from '@/assets/logo-celestial.png'
-import echoLogo from '@/assets/logo-quantum.png'
+import echoLogo from '@/assets/logo-echo.png'
 import pulseLogo from '@/assets/logo-pulse.png'
 import quantumLogo from '@/assets/logo-quantum.png'
+
+const logosImages = [
+  {
+    src: acmeLogo,
+    alt: 'acme logo',
+    className: 'logo-ticker-image',
+  },
+  {
+    src: apexLogo,
+    alt: 'apex logo',
+    className: 'logo-ticker-image',
+  },
+  {
+    src: celestialLogo,
+    alt: 'celestial logo',
+    className: 'logo-ticker-image',
+  },
+  {
+    src: echoLogo,
+    alt: 'echo logo',
+    className: 'logo-ticker-image',
+  },
+  {
+    src: pulseLogo,
+    alt: 'acme logo',
+    className: 'logo-ticker-image',
+  },
+  {
+    src: quantumLogo,
+    alt: 'acme logo',
+    className: 'logo-ticker-image',
+  },
+]
 
 function LogoTicker() {
   return (
     <div className='py-8 md:py-12 bg-white'>
       <div className='flex justify-center items-center'>
         <div className='flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]'>
-          <div className='flex gap-14 flex-none'>
-            <Image
-              src={acmeLogo}
-              alt='acme logo'
-              className='logo-ticker-image'
-            />
-            <Image
-              src={apexLogo}
-              alt='apex logo'
-              className='logo-ticker-image'
-            />
-            <Image
-              src={celestialLogo}
-              alt='celestial logo'
-              className='logo-ticker-image'
-            />
-            <Image
-              src={echoLogo}
-              alt='echo logo'
-              className='logo-ticker-image'
-            />
-            <Image
-              src={pulseLogo}
-              alt='pulse logo'
-              className='logo-ticker-image'
-            />
-            <Image
-              src={quantumLogo}
-              alt='quantum logo'
-              className='logo-ticker-image'
-            />
-          </div>
+          <motion.div
+            className='flex gap-14 flex-none pr-14'
+            animate={{ translateX: '-50%' }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              ease: 'linear',
+              repeatType: 'loop',
+            }}
+          >
+            {[...logosImages, ...logosImages].map(
+              ({ alt, className, src }, index) => (
+                <Image
+                  key={`${alt}-${index}`}
+                  src={src}
+                  alt={alt}
+                  className={className}
+                />
+              )
+            )}
+          </motion.div>
         </div>
       </div>
     </div>
