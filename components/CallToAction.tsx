2cd24065b9b617ada6858e4bd160e583418fa75c
Walison Theodoro <theodoro.walison@gmail.com>
2024-08-10 21:41:28 -0300

    feat: add call to action images animation

--- components/CallToAction.tsx
+++ components/CallToAction.tsx
@@ -1,11 +1,23 @@
-import Image from 'next/image'
+'use client'
+
+import { motion, useScroll, useTransform } from 'framer-motion'
 import ArrowRight from '@/assets/arrow-right.svg'
 import starImage from '@/assets/star.png'
 import springImage from '@/assets/spring.png'
+import { useRef } from 'react'
 
 const CallToAction = () => {
+  const callToActionRef = useRef(null)
+
+  const { scrollYProgress } = useScroll({
+    target: callToActionRef,
+    offset: ['start end', 'end start'],
+  })
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150])
+
   return (
-    <section className='bg-gradient-to-b from-[#ffffff] to-[#d2dcff] overflow-x-clip py-36'>
+    <section className='bg-gradient-to-b from-[#ffffff] to-[#d2dcff] overflow-x-clip pt-56 pb-36'>
       <div className='container flex flex-col items-center'>
         <div className='max-w-[600px] relative'>
           <h2 className='section-title'>Sign up for free today</h2>
@@ -13,17 +25,23 @@ const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
-          <Image
-            src={starImage}
+          <motion.img
+            src={starImage.src}
             alt='star image'
             width={360}
             className='absolute -left-[350px] -top-[137px]'
+            style={{
+              translateY,
+            }}
           />
-          <Image
-            src={springImage}
+          <motion.img
+            src={springImage.src}
             alt='spring image'
             width={360}
             className='absolute -right-[331px] -top-[19px]'
+            style={{
+              translateY,
+            }}
           />
         </div>
         <div className='mt-10 flex gap-2 justify-center'>
