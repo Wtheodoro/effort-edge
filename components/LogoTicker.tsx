89f2ed4543a7680590caf896a1ad1aeef38de380
Walison Theodoro <theodoro.walison@gmail.com>
2024-08-10 21:08:11 -0300

    feat: add product showcase images animation

--- components/LogoTicker.tsx
+++ components/LogoTicker.tsx
@@ -1,11 +1,6 @@
 'use client'
 
-import {
-  motion,
-  useScroll,
-  useTransform,
-  useMotionValueEvent,
-} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 import Image from 'next/image'
 import acmeLogo from '@/assets/logo-acme.png'
--- components/ProductShowcase.tsx
+++ components/ProductShowcase.tsx
@@ -1,11 +1,33 @@
+'use client'
+
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from 'framer-motion'
+
 import Image from 'next/image'
 import pyramidImage from '@/assets/pyramid.svg?url'
 import productImage from '@/assets/productImage.svg?url'
 import torusImage from '@/assets/torus.svg?url'
+import { useRef } from 'react'
 
 const ProductShowcase = () => {
+  const productShowcaseRef = useRef(null)
+
+  const { scrollYProgress } = useScroll({
+    target: productShowcaseRef,
+    offset: ['start end', 'end start'],
+  })
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150])
+
   return (
-    <section className='bg-gradient-to-b from-[#ffffff] to-[#d2dcff] py-24 overflow-x-clip'>
+    <section
+      ref={productShowcaseRef}
+      className='bg-gradient-to-b from-[#ffffff] to-[#d2dcff] py-24 overflow-x-clip'
+    >
       <div className='container'>
         <div className='max-w-[540px] mx-auto'>
           <div className='flex justify-center'>
@@ -23,18 +45,25 @@ const ProductShowcase = () => {
 
         <div className='relative'>
           <Image src={productImage} alt='Product image' className='mt-10' />
-          <Image
-            src={pyramidImage}
+          <motion.img
+            src={pyramidImage.src}
             alt='Pyramid image'
-            height={262}
             width={262}
+            height={262}
             className='hidden md:block md:absolute md:-right-36 md:-top-32 lg:top-20'
+            style={{
+              translateY,
+            }}
           />
-          <Image
-            src={torusImage}
+          <motion.img
+            src={torusImage.src}
             alt='Torus image'
+            width={248}
             height={248}
             className='hidden md:block md:absolute bottom-24 -left-36'
+            style={{
+              translateY,
+            }}
           />
         </div>
       </div>
